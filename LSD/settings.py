92d66bcccd933df07017386652410ac36ebde6b5
Shaitalee Vengurlekar <S.Vengurlekar@vertisystem.local>
2019-11-25 22:05:31 +0530

    files daded

--- LSD/settings.py
+++ LSD/settings.py
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR
